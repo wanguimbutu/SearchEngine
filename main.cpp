d5a488a552d06b0ba1aafbfeedc98627c367e419
wangui <92292540+wanguimbutu@users.noreply.github.com>
2023-09-08 09:30:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+int main(){
+   std::vector<std::string> documents ={"guide.txt","sample.txt"};
+   std::unordered_map<std::string, std::vector<std::string>>invertedIndex = buildInvertedIndex(documents);
 
+   for(const auto& entry : invertedIndex) {
+    std::cout <<entry.first << ": ";
+    for(const auto& document : entry.second){
+        std::cout << document << ": ";
+    }
+    std::cout <<std::endl;
+   }
+    
+       return 0;
+}
